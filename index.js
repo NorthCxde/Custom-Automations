1a963e928759d9b4e5af1739b04669e83a02753b
Code <loadingcode100@gmail.com>
2026-09-14 00:00:14 -0400

    Make sell, stats, and duel commands admin-only

--- index.js
+++ index.js
@@ -86,7 +86,10 @@ const ADMIN_ONLY_COMMAND_NAMES = new Set([
     'infractions',
     'unban',
     'deletemymessages',
-    'bank'
+    'bank',
+    'sell',
+    'stats',
+    'duel'
 ]);
 const DEFAULT_PUBLIC_COMMAND_NAMES = new Set(['profile', 'avatar', 'remind']);
 
