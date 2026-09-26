5d67ff277591be40101abb141dc3c9670ade10fa
Code <loadingcode100@gmail.com>
2026-09-26 09:51:15 -0400

    Ignore short numeric usernames in ticket scans

--- index.js
+++ index.js
@@ -4644,7 +4644,9 @@ client.scanTicketThread = async (thread, { force = false, interaction = null, tr
 
         const questionnaireValues = getTicketQuestionnaireValues(questionnaireMessages);
         const userIdCandidates = questionnaireValues.userIds.filter(userId => /^\d{7,}$/.test(userId));
-        const usernameCandidates = questionnaireValues.usernames.filter(username => username.length >= 5);
+        const usernameCandidates = questionnaireValues.usernames.filter(username =>
+            username.length >= 5 && !(/^\d+$/.test(username) && username.length < 7)
+        );
 
         if (!usernameCandidates.length && !userIdCandidates.length) return { sentCount: 0 };
 
